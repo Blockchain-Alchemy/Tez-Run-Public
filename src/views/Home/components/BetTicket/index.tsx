2097c51cfebef39d5c022a06e9a9ef59c65d647f
Eric Thomas <bluebird6922@gmail.com>
2022-03-17 02:38:38 +0300

    bet ticket

--- src/views/Home/components/BetTicket/index.tsx
+++ src/views/Home/components/BetTicket/index.tsx
@@ -14,7 +14,7 @@ function BetTicketCard({ storage, userAddress }) {
           return userBets?.get('1');
         })*/
         .then(raceBets => {
-          //console.log("raceBets", raceBets)
+          console.log("raceBets", raceBets)
           return raceBets?.map((ticket: any) => ({
             horseId: ticket.horseId.toNumber(),
             horseName: getHorseName(ticket.horseId.toNumber()),
@@ -23,15 +23,15 @@ function BetTicketCard({ storage, userAddress }) {
           }))
         })
         .then(tickets => {
-          //console.log("tickets", tickets)
+          console.log("tickets", tickets)
           tickets && setBetTickets(tickets)
         })
     }
   }, [storage, userAddress, setBetTickets])
 
-  /*useInterval(() => {
+  useInterval(() => {
     updateTickets()
-  }, 5000)*/
+  }, 5000)
 
   const getHorseName = (horseId) => {
     const horse = defaultHorses.find(it => it.id === horseId);
