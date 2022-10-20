c3bd2b6c2a196bbc4aa19ce747b5e9811acf8cda
Eric Thomas <bluebird6922@gmail.com>
2022-10-20 13:08:40 +0300

    fixed payout

--- src/views/Home/components/PlaceBet/index.tsx
+++ src/views/Home/components/PlaceBet/index.tsx
@@ -191,7 +191,7 @@ function PlaceBet({ raceState }) {
             className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
             placeholder="Place Bet"
             readOnly={true}
-            value={payout}
+            value={payout?.toFixed(4)}
           />
         </div>
       </div>
