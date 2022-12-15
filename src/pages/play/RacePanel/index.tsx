3833df1d5c53cb40644b794b1465c5b8fb5bcfa4
Eric Thomas <bluebird6922@gmail.com>
2022-12-15 09:29:53 +0300

    dependecy

--- src/pages/play/RacePanel/index.tsx
+++ src/pages/play/RacePanel/index.tsx
@@ -47,7 +47,7 @@ const RacePanel = ({ status, unityContext }) => {
     return () => {
       removeEventListener("FinishedRace", onFinishRace);
     };
-  }, [addEventListener, removeEventListener, onFinishRace]);
+  }, []);
 
   const handleTakeReward = async () => {
     console.log("TakeReward", address);
