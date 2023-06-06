366c9d249b9484ca5a16e75edb7a71f8ec3d318d
Eric Thomas <bluebird6922@gmail.com>
2023-06-06 17:58:00 +0900

    update contract with correct metadata

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -23,8 +23,8 @@ export const Networks: ConfigMap = {
   [NetworkType.MAINNET]: {
     RpcUrl: "https://mainnet.api.tez.ie",
     Indexer: "https://api.tzstats.com",
-    TezRun: "KT1FyWMVK57PtQMWi1VS2qEoNuGN5W895RgD",//"KT1Pn2u8gd9k5v37md7gKLbPxy9DwPzc2T4i",//"KT1SabQEuqzxrwp6caZyXPXpt98FhHoPB5NZ",
-    HorseCollection: "KT1Pik6AgZBWLGw8deWS4pV2RhLXVModjMjk",
+    TezRun: "KT1Pn2u8gd9k5v37md7gKLbPxy9DwPzc2T4i",//"KT1SabQEuqzxrwp6caZyXPXpt98FhHoPB5NZ",
+    HorseCollection: "KT1FyWMVK57PtQMWi1VS2qEoNuGN5W895RgD",
     Escrow: "KT1R2Uo6Q3o4emoPekgbEPs2eQMpqXfravSC",
     UUSD: "KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW",
   },
